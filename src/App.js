160b04d726b4a7e6a32577ab3b55e16801d66962
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2019-08-22 17:50:46 -0400

    MVP done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormikUserForm from "./components/Form"
+
+function App() {
+  return (
+    <div className="App">
+      <FormikUserForm/>
+    </div>
+  );
+}
+
+export default App;
